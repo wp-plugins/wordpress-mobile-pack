4e460c8d97b3054d715af5ccc2a1db4f3d2cadec
jamesgpearce <jamesgpearce@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-05-01 10:53:06 +0000

    Tagging official 1.0 release
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-mobile-pack/tags/1.0@114772 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugins/wpmp_barcode/wpmp_barcode_widget_admin.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+$Id: wpmp_barcode_widget_admin.php 8197 2009-04-29 06:49:22Z jpearce $
+
+Copyright (c) 2009 mTLD Top Level Domain Limited 
+
+Online support: http://mobiforge.com/forum/dotmobi/wordpress
+
+This file is part of the WordPress Mobile Pack.
+
+The WordPress Mobile Pack is Licensed under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with the
+License.
+
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+*/
+
+?>
+
+<p>
+  <label for="wpmp_barcode_title"><?php _e('Title:'); ?></label>
+  <?php print wpmp_barcode_option('wpmp_barcode_title', '', 'widefat'); ?>
+</p>
+<p>
+  <label for="wpmp_barcode_link"><?php _e('Link:'); ?></label>
+  <?php print wpmp_barcode_option('wpmp_barcode_link', '', 'widefat'); ?>
+</p>
+<p>
+  <label for="wpmp_barcode_size"><?php _e('Size:'); ?></label>
+  <br />
+  <?php print wpmp_barcode_option('wpmp_barcode_size', '', 'widefat', 'width:23%'); ?>px
+</p>
+<p>
+  <?php print wpmp_barcode_option('wpmp_barcode_help'); ?>
+  <label for="wpmp_barcode_help"><?php _e('Show explanation'); ?></label>
+</p>
+<p>
+  <?php print wpmp_barcode_option('wpmp_barcode_reader_list'); ?>
+  <label for="wpmp_barcode_reader_list"><?php _e('Show list of readers'); ?></label>
+</p>
+<input type="hidden" id="wpmp_barcode" name="wpmp_barcode" value="1" />
---
+++ wordpress_mobile_pack.php
@@ -0,0 +1,106 @@
+<?php
+
+/*
+$Id: wordpress_mobile_pack.php 8197 2009-04-29 06:49:22Z jpearce $
+
+Copyright (c) 2009 mTLD Top Level Domain Limited
+
+Online support: http://mobiforge.com/forum/dotmobi/wordpress
+
+This file is part of the WordPress Mobile Pack.
+
+The WordPress Mobile Pack is Licensed under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with the
+License.
+
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+*/
+
+/*
+Plugin Name: WordPress Mobile Pack
+Plugin URI: http://mobiforge.com/wordpress-mobile-pack
+Description: The dotMobi WordPress Mobile Pack is a complete toolkit to help mobilize your WordPress site and blog. It includes a <a href='themes.php?page=wpmp_switcher_admin'>mobile switcher</a>, <a href='themes.php?page=wpmp_theme_widget_admin'>filtered widgets</a>, and content adaptation for mobile device characteristics. Activating this plugin will also install a selection of mobile <a href='themes.php'>themes</a>. Also check out <a href='http://mobiforge.com/wordpress-mobile-pack' target='_blank'>the documentation</a> and <a href='http://mobiforge.com/forum/dotmobi/wordpress' target='_blank'>the forums</a>.
+Version: v1.0
+Author: James Pearce, dotMobi
+Author URI: http://www.mobiforge.com/users/james-pearce
+*/
+
+// you could disable sub-plugins here
+global $wpmp_plugins;
+$wpmp_plugins = array(
+  "wpmp_switcher",
+  "wpmp_barcode",
+  "wpmp_ads",
+  "wpmp_deviceatlas",
+  "wpmp_transcoder",
+);
+
+foreach($wpmp_plugins as $wpmp_plugin) {
+  if (file_exists($wpmp_plugin_file = dirname(__FILE__) . "/plugins/$wpmp_plugin/$wpmp_plugin.php")) {
+    include_once($wpmp_plugin_file);
+  }
+}
+
+register_activation_hook('wordpress_mobile_pack/wordpress_mobile_pack.php', 'wordpress_mobile_pack_activate');
+register_deactivation_hook('wordpress_mobile_pack/wordpress_mobile_pack.php', 'wordpress_mobile_pack_deactivate');
+add_action('admin_notices', 'wordpress_mobile_pack_admin_notices');
+
+function wordpress_mobile_pack_admin_notices() {
+  if($warning=get_option('wpmp_warning')) {
+    print "<div class='error'><p>$warning</p><p><small>(" . __('Deactivate and re-activate the WordPress Mobile Pack once resolved.') . ")</small></p></div>";
+  }
+}
+
+function wordpress_mobile_pack_activate() {
+  update_option('wpmp_warning', '');
+  wordpress_mobile_pack_soft_copy(dirname(__FILE__) . "/themes", get_theme_root());
+  wordpress_mobile_pack_hook('activate');
+}
+
+function wordpress_mobile_pack_soft_copy($source_dir, $destination_dir) {
+  if(file_exists($destination_dir) && !is_writable($destination_dir)) {
+    update_option('wpmp_warning', __('<strong>Could not install files</strong> to ') . $destination_dir . ('. Please ensure that the web server has write-access to that directory.'));
+    return;
+  }
+  if(!file_exists($destination_dir) || !is_dir($destination_dir)) {
+    mkdir($destination_dir, 0777, true);
+  }
+  foreach(scandir($source_dir) as $source_file) {
+    if ($source[0] == ".") {
+      continue;
+    }
+    if (file_exists($destination_child = "$destination_dir/$source_file")) {
+      continue;
+    }
+    if (is_dir($source_child = "$source_dir/$source_file")) {
+      wordpress_mobile_pack_soft_copy($source_child, $destination_child);
+      continue;
+    }
+    copy($source_child, $destination_child);
+  }
+}
+
+function wordpress_mobile_pack_deactivate() {
+  wordpress_mobile_pack_hook('deactivate');
+}
+
+function wordpress_mobile_pack_hook($action) {
+  global $wpmp_plugins;
+  foreach($wpmp_plugins as $wpmp_plugin) {
+    if (function_exists($function = $wpmp_plugin . "_" . $action)) {
+      call_user_func($function);
+    }
+  }
+}
+
+
+
+
+?>
