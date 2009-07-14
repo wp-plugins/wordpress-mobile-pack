dc51323f53c85553c94f3f935b2e327994d8459e
jamesgpearce <jamesgpearce@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-14 20:29:26 +0000

    warnings
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-mobile-pack/trunk@135122 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wordpress-mobile-pack.php
+++ wordpress-mobile-pack.php
@@ -29,10 +29,12 @@ specific language governing permissions and limitations under the License.
 Plugin Name: WordPress Mobile Pack
 Plugin URI: http://mobiforge.com/wordpress-mobile-pack
 Description: The dotMobi WordPress Mobile Pack is a complete toolkit to help mobilize your WordPress site and blog. It includes a <a href='themes.php?page=wpmp_switcher_admin'>mobile switcher</a>, <a href='themes.php?page=wpmp_theme_widget_admin'>filtered widgets</a>, and content adaptation for mobile device characteristics. Activating this plugin will also install a selection of mobile <a href='themes.php'>themes</a>. Also check out <a href='http://mobiforge.com/wordpress-mobile-pack' target='_blank'>the documentation</a> and <a href='http://mobiforge.com/forum/dotmobi/wordpress' target='_blank'>the forums</a>.
-Version: 1.1
+Version: 1.1.1
 Author: James Pearce, dotMobi
 Author URI: http://www.mobiforge.com/users/james-pearce
 */
+
+define('WPMP_VERSION', '1.1.1');
 
 // you could disable sub-plugins here
 global $wpmp_plugins;
@@ -56,9 +58,24 @@ register_activation_hook('wordpress-mobile-pack/wordpress-mobile-pack.php', 'wor
 register_deactivation_hook('wordpress-mobile-pack/wordpress-mobile-pack.php', 'wordpress_mobile_pack_deactivate');
 add_action('admin_notices', 'wordpress_mobile_pack_admin_notices');
 
+add_action('send_headers', 'wordpress_mobile_pack_send_headers');
+add_filter('get_the_generator_xhtml', 'wordpress_mobile_pack_generator');
+add_filter('get_the_generator_html', 'wordpress_mobile_pack_generator');
+
+function wordpress_mobile_pack_send_headers($wp) {
+  @header("X-Mobilized-By: WordPress Mobile Pack " . WPMP_VERSION);
+}
+function wordpress_mobile_pack_generator($generator) {
+  return '<meta name="generator" content="WordPress ' . get_bloginfo( 'version' ) . ', fitted with the WordPress Mobile Pack ' . WPMP_VERSION . '" />';
+}
+
 function wordpress_mobile_pack_admin_notices() {
   if($warning=get_option('wpmp_warning')) {
     print "<div class='error'><p><strong style='color:#770000'>Critical WordPress Mobile Pack Issue</strong></p><p>$warning</p><p><small>(" . __('Deactivate and re-activate the WordPress Mobile Pack once resolved.') . ")</small></p></div>";
+  }
+  if($flash=get_option('wpmp_flash')) {
+    print "<div class='error'><p><strong style='color:#770000'>Important WordPress Mobile Pack Notice</strong></p><p>$flash</p></div>";
+    update_option('wpmp_flash', '');
   }
 }
 
@@ -73,11 +90,6 @@ function wordpress_mobile_pack_activate() {
 function wordpress_mobile_pack_readiness_audit() {
   $ready = true;
   $why_not = array();
-
-//  if (version_compare(PHP_VERSION, '5.0.0', '<')) {
-//    $ready = false;
-//    $why_not[] = __('<strong>PHP version not compatible.</strong> PHP version 5 is required for this plugin, and you have version ') . PHP_VERSION;
-//  }
 
   if (version_compare(PHP_VERSION, '6.0.0', '>=')) {
     $ready = false;
@@ -107,7 +119,7 @@ function wordpress_mobile_pack_readiness_audit() {
 function wordpress_mobile_pack_soft_copy($source_dir, $destination_dir) {
   if(file_exists($destination_dir)) {
     if (!is_writable($destination_dir) || !is_executable($destination_dir)) {
-      update_option('wpmp_warning', __('<strong>Could not install theme files</strong> to ') . $destination_dir . ('. Please ensure that the web server has write- and execute-access to that directory.'));
+      update_option('wpmp_warning', __('<strong>Could not install files</strong> to ') . $destination_dir . ('. Please ensure that the web server has write- and execute-access to that directory.'));
       return;
     }
   } elseif (!is_dir($destination_dir)) {
@@ -120,6 +132,7 @@ function wordpress_mobile_pack_soft_copy($source_dir, $destination_dir) {
       continue;
     }
     if (file_exists($destination_child = "$destination_dir/$source_file")) {
+      update_option('wpmp_flash', "<strong>A file already existed</strong> at $destination_dir/$source_file. It may have been installed by a previous version of this plugin and so it was not overwritten.<ul><li>You should remove the existing mobile theme folders if you want to fully reset or upgrade them.</li><li>Take precautions if you have manually edited any theme files - your changes should be re-applied.</li><li>De-activate and re-activate the plugin to complete the installation of the themes.</li></ul>");
       continue;
     }
     if (is_dir($source_child = "$source_dir/$source_file")) {
