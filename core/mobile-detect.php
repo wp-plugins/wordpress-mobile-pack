1229367e1005db6494040177ba7033c1d18dcd7a
cborodescu <cborodescu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-17 09:04:34 +0000

    Version 2.1 with support for pages and connect to premium
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-mobile-pack/tags/2.1@991715 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ core/mobile-detect.php
@@ -0,0 +1,57 @@
+<?php
+
+if ( ! class_exists( 'WPMobileDetect' ) ) {
+    
+    /**
+     * WPMobileDetect
+     * 
+     * Main class for detecting the user's device and browser.
+     * 
+     */  
+    class WPMobileDetect {
+    
+    
+    	/* ----------------------------------*/
+        /* Methods							 */
+        /* ----------------------------------*/
+    	
+    	function wmp_detect_device() { 
+    		
+    		// set load app variable
+    		$load_app = false;
+    		
+    		require_once (WMP_PLUGIN_PATH.'libs/mobileesp/mdetect.php');
+    		$uagentObj = new UAgentInfo();
+    		 
+    		$is_tablet = $uagentObj->DetectTierTablet();
+    		$is_mobile = $uagentObj->DetectMobileQuick();
+    		$is_webkit = $uagentObj->DetectWebkit();
+    		$is_windows_mobile = $uagentObj->DetectWindowsPhone8();
+
+    		$is_firefox_os = $uagentObj->DetectFirefoxOsPhone();
+			$is_firefox = $uagentObj->DetectFirefoxPhone();
+    		
+			if($is_mobile && !$is_tablet && ($is_webkit || $is_windows_mobile || $is_firefox_os || $is_firefox)) {
+
+    			
+    			// set load app cookie	
+    			setcookie("wmp_load_app", 1, time()+3600*7*24,'/');
+    			
+    			// set load app variable to true
+    			$load_app = true;	
+    		}
+    		
+    		return $load_app;
+    	}
+		
+		
+		function wmp_is_tablet() {
+			
+			require_once (WMP_PLUGIN_PATH.'libs/mobileesp/mdetect.php');
+    		$uagentObj = new UAgentInfo();
+			
+			return $uagentObj->DetectTierTablet();
+		}
+		
+    }
+}
