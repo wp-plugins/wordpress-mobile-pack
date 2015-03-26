12cfbd3cb47c3e1bbfbfe936429307243608c934
cborodescu <cborodescu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-26 10:31:32 +0000

    Added rel=canonical and rel="alternate", fixed bugs
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-mobile-pack/tags/2.1.2@1121235 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/sections/wmp-show-mobile.php
+++
@@ -1,3 +0,0 @@
-<div id="show-mobile" style="width:100%; text-align: center;">
-	<a href="<?php echo get_bloginfo('url');?>?wmp_theme_mode=mobile" title="Switch to mobile version">Switch to mobile version</a>
-</div>
